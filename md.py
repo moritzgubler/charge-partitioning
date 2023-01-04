7f1c81c19a13d8b1c0756f017795b6b06f92d232
moritzgubler <moritz.gubler@gmail.com>
2023-01-04 17:41:52 +0100

    adjusted md

--- md.py
+++ md.py
@@ -31,54 +31,52 @@ mol.charge = totalCharge
 mol.build()
 
 print("\n\nStart DFT calculation")
-# mf = mol.UHF().run()
+mf = mol.UHF().run()
 
 
 grad_scan = pyscf.scf.UHF(mol).nuc_grad_method().as_scanner()
 
+f = open('traj.xyz', mode='w')
+veloc = np.zeros((26,3))
+veloc[0, :] = (mol.atom_coord(1) - mol.atom_coord(0))
+veloc[0, :] = veloc[0, :] / np.linalg.norm(veloc[0, :]) * 0.001
+veloc[1, :] = - veloc[0, :]
+integrator = md.NVE(mf, dt=40, steps=1000, trajectory_output=f ).run(veloc=veloc)
+f.close()
 
-import sqnm
+# import sqnm
 
-opt = sqnm.SQNM(3*26, 10, -.1, 1e-3, 1e-2)
+# opt = sqnm.SQNM(3*26, 10, -.1, 1e-3, 1e-2)
 
-for i in range(30):
-    e, g = grad_scan(mol)
-    x = mol.atom_coords()
-    xt = x[:,:]
-    gt = g[:, :]
-    xt = np.reshape(xt, 26*3)
-    gt = np.reshape(gt, 26*3)
-    xt = xt + opt.sqnm_step(xt, e, gt)
-    xt = np.reshape(xt, (26,3))
+# for i in range(30):
+#     e, g = grad_scan(mol)
+#     x = mol.atom_coords()
+#     xt = x[:,:]
+#     gt = g[:, :]
+#     xt = np.reshape(xt, 26*3)
+#     gt = np.reshape(gt, 26*3)
+#     xt = xt + opt.sqnm_step(xt, e, gt)
+#     xt = np.reshape(xt, (26,3))
 
-    x[:, :] = xt[:, :]
+#     x[:, :] = xt[:, :]
 
-    atom = []
+#     atom = []
 
-    for i in range(26):
-        atom.append(( mol._atom[i][0] ,list(x[i, :])))
+#     for i in range(26):
+#         atom.append(( mol._atom[i][0] ,list(x[i, :])))
 
-    mol = pyscf.gto.Mole()
+#     mol = pyscf.gto.Mole()
 
-    mol.atom = atom
-    mol.basis = 'sto-3g'
-    # mol.basis = 'ccpvdz'
-    mol.unit = 'B'
-    mol.symmetry = False
-    mol.charge = totalCharge    
-    mol.build()
-    for i in range(26):
-        print(mol._atom[i][0], *np.array(mol._atom[i][1]) * 0.52918)
-
-
+#     mol.atom = atom
+#     mol.basis = 'sto-3g'
+#     # mol.basis = 'ccpvdz'
+#     mol.unit = 'B'
+#     mol.symmetry = False
+#     mol.charge = totalCharge    
+#     mol.build()
+#     for i in range(26):
+#         print(mol._atom[i][0], *np.array(mol._atom[i][1]) * 0.52918)
 
 
 
 
-
-
-
-
-# f = open('toto.xyz', mode='w')
-# integrator = md.NVE(dft_res, dt=40, steps=1000, trajectory_output=f ).run()
-# f.close()
