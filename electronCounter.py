b15e1011841b1cb1f1ea7dd587ee7420e4140a30
moritzgubler <moritz.gubler@gmail.com>
2023-01-04 17:43:16 +0100

    added electron counter

---
+++ electronCounter.py
@@ -0,0 +1,14 @@
+import pyscf
+import periodictable
+
+def countElectrons(mol: pyscf.gto.Mole):
+    symbs, coords = zip(*mol._atom)
+    totalElectrons = 0
+    totalCoreElectrons = 0
+    totalValenceElectrons = 0
+    for symb in symbs:
+        coreElectrons, valenceElectrons = periodictable.getCoreAndValenceElectrons(symb)
+        totalElectrons += coreElectrons + valenceElectrons
+        totalCoreElectrons += coreElectrons
+        totalValenceElectrons += valenceElectrons
+    return totalElectrons, totalCoreElectrons, totalValenceElectrons
