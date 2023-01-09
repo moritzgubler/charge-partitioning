92d123408a6404e4097932b51a48bd17bd773ae4
moritzgubler <moritz.gubler@gmail.com>
2023-01-09 16:18:23 +0100

    corrected bug

--- dc_dft.py
+++ dc_dft.py
@@ -9,11 +9,10 @@ def get_dc_energy(mol, hf_calc, isRestricted=True, gridLevel=5):
     dm_hf = hf_calc.make_rdm1(ao_repr=False)
     vj, vk = hf_calc.get_jk(mol, dm_hf)
 
-    exh = - numpy.einsum('ij,ji->', vk, dm_hf) / 4
-
     if not isRestricted:
         dm_hf = dm_hf[0] + dm_hf[1]
 
+    exh = - numpy.einsum('ij,ji->', vk, dm_hf) / 4
 
     ni = dft.numint.NumInt()
     nelec, xc_energy, vxc = ni.get_vxc(mol=mol, grids=grid, xc_code='SCAN,SCAN', dms=dm_hf)
