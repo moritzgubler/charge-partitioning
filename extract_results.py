7706b6ac87769f7a33767946fc44410bcbbfcfd5
moritzgubler <moritz.gubler@gmail.com>
2024-03-12 13:07:18 +0100

    add energy  plot (dont know that this does)

--- extract_results.py
+++ extract_results.py
@@ -41,7 +41,7 @@ def main():
     for fun in functionals:
         charge_diff[fun] = np.hstack(charges[fun]) - np.hstack(charges['cc'])
 
-    print('\nrmse e rho diff (hirshfeld charges)')
+    print('\nrmse hirshfeld charges')
     n_charges = len(charge_diff['cc'])
     for fun in functionals:
         print(fun, 1/n_charges * np.linalg.norm(charge_diff[fun]))
@@ -67,6 +67,45 @@ def main():
     print("\naverage quadrupole error:")
     for fun in functionals:
         print(fun, np.mean(quad_diff[fun]))
+
+    # make energy plot:
+    energies = get_key_result(dict_list, 'etot')
+    energies['dc_dft'] = []
+    for dictionary in dict_list:
+        energies['dc_dft'].append(dictionary['results']['hf']['df_dft'])
+
+    # f_copy = functionals.copy()
+    # f_copy.remove('cc')
+    # f_copy.append('dc_dft')
+    f_copy = ['scan', 'SCAN0,SCAN', 'b3lyp']
+    for fun in f_copy:
+        plt.scatter(energies['cc'], energies[fun], label = fun)
+    plt.legend()
+    plt.xlabel('cc energies')
+    plt.ylabel('energy')
+    plt.title('energy correlation')
+    plt.grid()
+    plt.savefig('energy_correlation.pdf')
+    plt.show()
+    for fun in f_copy:
+        plt.scatter(energies['cc'], np.array(energies[fun]) - np.array(energies['cc']), label = fun)
+    plt.legend()
+    plt.xlabel('cc energies')
+    plt.ylabel('E_DFT - E_CC')
+    plt.title('energy error')
+    plt.grid()
+    plt.savefig('energy_error.pdf')
+    plt.show()
+
+    # f_copy.remove('dc_dft')
+
+    for fun in f_copy:
+        plt.scatter(np.hstack(charges['cc']), np.hstack(charges[fun]) - np.hstack(charges['cc']), label = fun)
+    plt.grid()
+    plt.legend()
+    plt.xlabel('CC charges')
+    plt.ylabel('charge_dft - charge_cc')
+    plt.show()
     
 
 def get_key_result(dict_list, key):
